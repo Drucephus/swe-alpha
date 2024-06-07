28031a9a0e39c663a9102eed41e87ef52c9faca6
Andrew Courtney <acourtney09@gmail.com>
2024-06-07 19:08:29 +0000

    Two functions commited. Get_board() and print_board(board)

--- sudoku_sprint.py
+++ sudoku_sprint.py
@@ -1,5 +1,5 @@
-def get_board()
-    # Get user input for Sudoku puzzle ('.' denotes empty cells)
+def get_board():
+    # Get user input for Sudoku puzzle ('.' denotes an empty cell). Returns a board
     board = []
     print("Enter the Sudoku puzzle row by row (use '.' for empty cells):")
     for i in range(9):
@@ -14,11 +14,21 @@ def get_board()
 
 def print_board(board):
     """Function to print the Sudoku board."""
-    for row in board:
-        print(" ".join(str(num) if num != 0 else '.' for num in row))
+    if board != []:
+        for i in range(9):
+            if i % 3 == 0:
+                print("+-------+-------+-------+")
+            for j in range(9):
+                if j % 3 == 0:
+                    print("| ", end="")
+                print(str(board[i][j]) if board[i][j] != 0 else ".", end=" ")
+            print("|")
+        print("+-------+-------+-------+")
+    else:
+        print("The board is empty :(")
     print()
 
-def main()
+def main():
     """Main function."""
     board = get_board()
     print_board(board)
